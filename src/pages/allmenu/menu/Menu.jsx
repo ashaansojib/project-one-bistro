f9eb72aa10a6abdc0953adbdf90fafa152dff91b
ashaansojib <ashaduzzamansojib67@g.gmail.com>
2023-06-01 02:06:41 +0600

    add bistro menu

---
+++ src/pages/allmenu/menu/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuBanner from '../banner/MenuBanner';
+
+const Menu = () => {
+    return (
+        <div>
+            <MenuBanner></MenuBanner>
+        </div>
+    );
+};
+
+export default Menu;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../pages/main/Main";
 import Home from "../pages/home/Home";
 import Error from "../pages/error/Error";
+import Menu from "../pages/allmenu/menu/Menu";
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/ourmenu',
+                element: <Menu></Menu>
             }
         ]
     },
